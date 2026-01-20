c13a2058c67583d6a2cc6e92529f58e4c397529d
Ekler25 <134164161+ekler25@users.noreply.github.com>
2026-01-20 13:44:04 +0100

    [U] instructions

--- include/classes/instruction.hpp
+++ include/classes/instruction.hpp
@@ -3,43 +3,33 @@
 #include <types.hpp>
 
 enum class OpCodes {
-    JMP, IPUSH, CALL, INVH,
-    RET, IPOP, IADD, ISUB, IMUL, IDIV,
-    PUSHSTR
+    JMP, CALL, INVH,
+    RET, EOP, IPOP, IADD, ISUB, IMUL, IDIV,
+    PUSHSTR, IPUSH, CPUSH
 };
 
 const Map<OpCodes, int> OpCodeTable = {
-    {OpCodes::IPOP, 0},
     {OpCodes::IADD, 0},
+    {OpCodes::IPOP, 0},
     {OpCodes::ISUB, 0},
     {OpCodes::IMUL, 0},
     {OpCodes::IDIV, 0},
     {OpCodes::RET,  0},
     {OpCodes::EOP,  0},
 
-    {OpCodes::JMP,  1},
-    {OpCodes::IPUSH,1},
-    {OpCodes::CALL, 1},
-    {OpCodes::INVH, 1},
-
+    {OpCodes::JMP,   1},
+    {OpCodes::CPUSH, 1},
+    {OpCodes::CALL,  1},
+    {OpCodes::INVH,  1},
+    
+    {OpCodes::IPUSH, -1},
     {OpCodes::PUSHSTR, -1}
 };
 
 struct Instruction {
     OpCodes OpCode;
     Vec<Byte> Operands;
-
-    Instruction(OpCodes OpCode, const Vec<Byte>& Operands = {})
-        : OpCode(OpCode), Operands(Operands) {}
+    
+    Instruction(OpCodes OpCode, Vec<Byte>& Operands)
+    : OpCode(OpCode), Operands(Operands) {}
 };
-
-void JMP(int& Index, Vec<Byte>& Operands, const Vec<Instruction>& Inst);
-void IPUSH(int& Index, Vec<Byte>& Operands, const Vec<Instruction>& Inst);
-void CALL(int& Index, Vec<Byte>& Operands, const Vec<Instruction>& Inst);
-void RET(int& Index, Vec<Byte>& Operands, const Vec<Instruction>& Inst);
-void EOP(int& Index, Vec<Byte>& Operands, const Vec<Instruction>& Inst);
-void IPOP(int& Index, Vec<Byte>& Operands, const Vec<Instruction>& Inst);
-void IADD(int& Index, Vec<Byte>& Operands, const Vec<Instruction>& Inst);
-void ISUB(int& Index, Vec<Byte>& Operands, const Vec<Instruction>& Inst);
-void IMUL(int& Index, Vec<Byte>& Operands, const Vec<Instruction>& Inst);
-void IDIV(int& Index, Vec<Byte>& Operands, const Vec<Instruction>& Inst);
