15a771976367f4ef68a204c19e3fda1171f5368a
Ekler25 <134164161+ekler25@users.noreply.github.com>
2026-01-20 13:43:43 +0100

    [U] machine class

--- include/classes/machine.hpp
+++ include/classes/machine.hpp
@@ -10,7 +10,22 @@ struct Machine {
     Vec<StackFrame> CallStack;
     Vec<Value> OperandStack;
     Vec<UPtr<String>> StringHeap;
+
+    int IP = 0;
     
     Vec<Instruction> Decode(Vec<Byte>& Bytecode);
     void Execute(Vec<Instruction> Inst);
+
+    void JMP(Vec<Byte>& Operands);
+    void CALL(Vec<Byte>& Operands);
+    void RET(Vec<Byte>& Operands);
+    void INVH(Vec<Byte>& Operands);
+    void EOP(Vec<Byte>& Operands);
+    void IPOP(Vec<Byte>& Operands);
+    void IADD(Vec<Byte>& Operands);
+    void ISUB(Vec<Byte>& Operands);
+    void IMUL(Vec<Byte>& Operands);
+    void IDIV(Vec<Byte>& Operands);
+    void IPUSH(Vec<Byte>& Operands);
+    void CPUSH(Vec<Byte>& Operands);
 };
