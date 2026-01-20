7d8be3b75b0176b762dec04c4b5ca700a3e2893d
Ekler25 <134164161+ekler25@users.noreply.github.com>
2026-01-20 13:41:04 +0100

    [U] JMP, IPUSH, CPUSH, INVH

--- src/classes/machine/instructions.cpp
+++ src/classes/machine/instructions.cpp
@@ -1,43 +1,75 @@
 #include <iostream>
 #include <classes/machine.hpp>
+#include <classes/value.hpp>
 #include <types.hpp>
+#include <utils.hpp>
 
-void JMP(int& Index, Vec<Byte>& Operands, const Vec<Instruction>& Inst) {
+void Machine::JMP(Vec<Byte>& Operands) {
+    IP += Operands[0];
+}
+
+void Machine::IPUSH(Vec<Byte>& Operands) {
+    if (Operands.empty())
+        throw RuntimeError("[ \x1b[91mRuntime Error\x1b[0m ] Failed to execute ipush : no value provieded!\n");
+
+    Value Val;
+    Val.InitInt(BytesToInt(Operands));
+
+    OperandStack.push_back(Val);
+}
+
+void Machine::CALL(Vec<Byte>& Operands) {
 
 }
 
-void IPUSH(int& Index, Vec<Byte>& Operands, const Vec<Instruction>& Inst) {
+void Machine::RET(Vec<Byte>& Operands) {
 
 }
 
-void CALL(int& Index, Vec<Byte>& Operands, const Vec<Instruction>& Inst) {
+void Machine::INVH(Vec<Byte>& Operands) {
+    switch (Operands[0]) {
+        case 0: return;
+        case 1:
+            if (OperandStack.size() > 0) {
+                Value Output = OperandStack.back();
+                if (Output.Type == ValueType::Char) std::cout << Output.c;
+                OperandStack.pop_back();
+            }
+            break;
+        default: break;
+    }
+}
+
+void Machine::EOP(Vec<Byte>& Operands) {
 
 }
 
-void RET(int& Index, Vec<Byte>& Operands, const Vec<Instruction>& Inst) {
+void Machine::IPOP(Vec<Byte>& Operands) {
 
 }
 
-void EOP(int& Index, Vec<Byte>& Operands, const Vec<Instruction>& Inst) {
+void Machine::IADD(Vec<Byte>& Operands) {
 
 }
 
-void IPOP(int& Index, Vec<Byte>& Operands, const Vec<Instruction>& Inst) {
+void Machine::ISUB(Vec<Byte>& Operands) {
 
 }
 
-void IADD(int& Index, Vec<Byte>& Operands, const Vec<Instruction>& Inst) {
+void Machine::IMUL(Vec<Byte>& Operands) {
 
 }
 
-void ISUB(int& Index, Vec<Byte>& Operands, const Vec<Instruction>& Inst) {
+void Machine::IDIV(Vec<Byte>& Operands) {
 
 }
 
-void IMUL(int& Index, Vec<Byte>& Operands, const Vec<Instruction>& Inst) {
+void Machine::CPUSH(Vec<Byte>& Operands) {
+    if (Operands.empty())
+        throw RuntimeError("[\x1b[91mRuntime Error\x1b[0m] Failed to execute cpush : no value provided!\n");
 
-}
+    Value Val;
+    Val.InitChar((char)Operands[0]);
 
-void IDIV(int& Index, Vec<Byte>& Operands, const Vec<Instruction>& Inst) {
-    
+    OperandStack.push_back(Val);
 }
