4a22353f8e943c1f59d41f00d4308bf91746982d
Ekler25 <134164161+ekler25@users.noreply.github.com>
2026-01-23 14:04:31 +0100

    Update utils.hpp

--- include/utils.hpp
+++ include/utils.hpp
@@ -1,9 +1,16 @@
 #include <types.hpp>
 
+static constexpr Byte EVMVersion = 0x00;
+
 enum class InvokeHost {
     Terminate,
     StdOut,
     StdIn,
 };
 
+enum class Error {
+    File, Fetching, Decoding, Header, Execution, Usage
+};
+
 Qword BytesToInt(const Vec<Byte>& Bytes);
+void Err(Error ErrorType, String Value);
