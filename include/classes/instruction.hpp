86690041921ab1c7f740b4cba40da3032e0d3509
Ekler25 <134164161+ekler25@users.noreply.github.com>
2026-01-23 14:06:15 +0100

    instructions

--- include/classes/instruction.hpp
+++ include/classes/instruction.hpp
@@ -5,7 +5,8 @@
 enum class OpCodes {
     JMP, CALL, INVH,
     RET, EOP, IPOP, IADD, ISUB, IMUL, IDIV,
-    SPUSH, IPUSH, CPUSH
+    SPUSH, IPUSH, CPUSH,
+    STOREG, STOREL, LOADG, LOADL
 };
 
 const Map<OpCodes, int> OpCodeTable = {
@@ -17,18 +18,23 @@ const Map<OpCodes, int> OpCodeTable = {
     {OpCodes::RET,  0},
     {OpCodes::EOP,  0},
 
-    {OpCodes::JMP,   1},
-    {OpCodes::CPUSH, 1},
-    {OpCodes::CALL,  1},
-    {OpCodes::INVH,  1},
-    
-    {OpCodes::IPUSH, -1},
-    {OpCodes::SPUSH, -1}
+    {OpCodes::JMP,    1},
+    {OpCodes::CPUSH,  1},
+    {OpCodes::CALL,   1},
+    {OpCodes::INVH,   1},
+
+    {OpCodes::IPUSH,  -1},
+    {OpCodes::SPUSH,  -1},
+    {OpCodes::STOREG, -1},
+    {OpCodes::STOREL, -1},
+    {OpCodes::LOADG,  -1},
+    {OpCodes::LOADL,  -1},
 };
 
 struct Instruction {
     OpCodes OpCode;
     Vec<Byte> Operands;
+    int Offset;
     
     Instruction(OpCodes OpCode, Vec<Byte>& Operands)
     : OpCode(OpCode), Operands(Operands) {}
