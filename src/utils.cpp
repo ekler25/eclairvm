dbfa3eb523e6449104330c0d60c4f8647c6a9a33
Ekler25 <134164161+ekler25@users.noreply.github.com>
2026-01-23 14:03:58 +0100

    Update utils.cpp

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,4 +1,6 @@
+#include <iostream>
 #include <types.hpp>
+#include <utils.hpp>
 
 Qword BytesToInt(const Vec<Byte>& Bytes) {
     Qword Value = 0;
@@ -15,4 +17,17 @@ Qword BytesToInt(const Vec<Byte>& Bytes) {
     }
 
     return Value;
+}
+
+void Err(Error ErrorType, String Value) {
+    switch (ErrorType) {
+        case Error::File:       std::cout << "[ \x1b[91mFile Error\x1b[0m ] "      << Value << "\n"; break;
+        case Error::Fetching:   std::cout << "[ \x1b[91mFetching Error\x1b[0m ] "  << Value << "\n"; break;
+        case Error::Decoding:   std::cout << "[ \x1b[91mDecoding Error\x1b[0m ] "  << Value << "\n"; break;
+        case Error::Header:     std::cout << "[ \x1b[91mHeader Error\x1b[0m ] "    << Value << "\n"; break;
+        case Error::Execution:  std::cout << "[ \x1b[91mRuntime Error\x1b[0m ] "   << Value << "\n"; break;
+        case Error::Usage:      std::cout << "[ \x1b[91mUsage Error\x1b[0m ] "     << Value << "\n"; break;
+        default:                std::cout << "Unknown error has occured! EVM terminated\n"; break;
+    }
+    std::exit(1);
 }
