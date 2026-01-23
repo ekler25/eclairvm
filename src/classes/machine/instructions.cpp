0544aff2457aa0d755791432f5f240d2e5ab2caa
Ekler25 <134164161+ekler25@users.noreply.github.com>
2026-01-23 14:02:30 +0100

    [U] basic CALL and RET
    also i'm switching from std::runtime_error to custom error function

--- src/classes/machine/instructions.cpp
+++ src/classes/machine/instructions.cpp
@@ -19,16 +19,42 @@ void Machine::IPUSH(Vec<Byte>& Operands) {
 }
 
 void Machine::CALL(Vec<Byte>& Operands) {
+    if (Operands.empty())
+        Err(Error::Execution, "Function identifier not specified");
 
+    Function& Fn = GlobalFunctionTable[Operands[0]];
+
+    if (OperandStack.size() < Fn.Args)
+        Err(Error::Execution, "Not enough arguments for function call");
+
+    uint32 VarPtr = OperandStack.size() - Fn.Args;
+
+    OperandStack.resize(VarPtr + Fn.Args + Fn.Locals);
+
+    CallStack.emplace_back(
+        IP + 1,
+        VarPtr,
+        Fn.Args + Fn.Locals 
+    );
+
+    IP = Fn.Address;
 }
 
 void Machine::RET(Vec<Byte>& Operands) {
+    if (CallStack.empty())
+        Err(Error::Execution, "RET cannot be executed on an empty callstack");
 
+    CallStackFrame& Frame = CallStack.back();
+
+    OperandStack.resize(Frame.VarPtr);
+
+    IP = Frame.ReturnAddress;
+    CallStack.pop_back();
 }
 
 void Machine::INVH(Vec<Byte>& Operands) {
     switch (Operands[0]) {
-        case 0: return;
+        case 0: std::exit(0);
         case 1:
             if (OperandStack.size() > 0) {
                 Value Output = OperandStack.back();
@@ -38,6 +64,8 @@ void Machine::INVH(Vec<Byte>& Operands) {
                 OperandStack.pop_back();
             }
             break;
+        case 3: std::cout << "\n"; break;
+        case 255: std::exit(1);
         default: break;
     }
 }
@@ -138,3 +166,36 @@ void Machine::SPUSH(Vec<Byte>& Operands) {
     OperandStack.push_back(Val);
 }
 
+void Machine::STOREG(Vec<Byte>& Operands) {
+    if (OperandStack.size() < 1) return;
+    if (Operands.empty())
+        throw RuntimeError("[\x1b[91mRuntime Error\x1b[0m] Failed to execute storeg : no index provided!\n");
+
+    if (GlobalVariableTable.size() <= BytesToInt(Operands))
+        GlobalVariableTable.resize(BytesToInt(Operands) + 1); 
+
+    Value Val = OperandStack.back();
+    OperandStack.pop_back();
+
+    GlobalVariableTable[BytesToInt(Operands)] = Val;
+}
+
+void Machine::STOREL(Vec<Byte>& Operands) {
+
+}
+
+void Machine::LOADG(Vec<Byte>& Operands) {
+    if (Operands.empty())
+        throw RuntimeError("[\x1b[91mRuntime Error\x1b[0m] Failed to execute loadg : no index provided!\n");
+
+    if (GlobalVariableTable.size() <= BytesToInt(Operands))
+        throw RuntimeError("[\x1b[91mRuntime Error\x1b[0m] Failed to execute loadg : invalid index!\n");
+
+    Value Val = GlobalVariableTable[BytesToInt(Operands)];
+
+    OperandStack.push_back(Val);
+}
+
+void Machine::LOADL(Vec<Byte>& Operands) {
+
+}
