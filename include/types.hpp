f68961a2879635f8625416c8269895ae72dad8dc
Ekler25 <134164161+ekler25@users.noreply.github.com>
2026-01-23 14:04:18 +0100

    types.hpp

--- include/types.hpp
+++ include/types.hpp
@@ -14,6 +14,11 @@ using Word  = uint16_t;
 using Dword = uint32_t;
 using Qword = uint64_t;
 
+using uint8  = uint8_t;
+using uint16 = uint16_t;
+using uint32 = uint32_t;
+using uint64 = uint64_t;
+
 using int8  = int8_t;
 using int16  = int16_t;
 using int32  = int32_t;
