b42a21247b7f6f395089dcf952a6035834f5bf77
Ekler25 <134164161+ekler25@users.noreply.github.com>
2026-01-23 14:05:59 +0100

    call stack frame and function class

--- include/classes/frame.hpp
+++ include/classes/frame.hpp
@@ -1,10 +1,22 @@
 #pragma once
 
 #include <classes/instruction.hpp>
+#include <classes/value.hpp>
 #include <types.hpp>
 
-struct StackFrame {
-    int VarIndex;
-    int VarReserve;
-    int ReturnTo;
+struct Function {
+    uint32 Address;
+    uint32 Locals;
+    uint16 Args;
+
+    Function() = default;
+    Function(uint32 Ad, uint32 Lo, uint16 Ar) : Address(Ad), Locals(Lo), Args(Ar) {}
+};
+
+struct CallStackFrame {
+    uint32 ReturnAddress;
+    uint32 VarPtr;
+    uint32 VarSlots;
+
+    CallStackFrame(uint32 Ra, uint32 Vp, uint32 Vs) : ReturnAddress(Ra), VarPtr(Vp), VarSlots(Vs) {}
 };
